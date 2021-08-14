0114904c36f2427d93fdfe42473497e0da9914d9
Roshanak <rosh.agahi@gmail.com>
2021-08-14 14:53:33 -0700

    wrap app in provider, make enhancer

--- src/index.js
+++ src/index.js
@@ -3,9 +3,18 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
 
-import {createStore, applyMiddleware} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
- 
+//gives access to dev tools
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(r, composeEnhancers(applyMiddleware(thunk)))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
