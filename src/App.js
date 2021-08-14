1d7ed60d2944f358ab2da2435da330057ca569f1
Roshanak <rosh.agahi@gmail.com>
2021-08-14 14:06:17 -0700

    including redux, react-redux, redux-thunk, react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 // import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://127.0.0.1:3000/api/v1/challenges', {
+      method: 'GET'
+    })
+    .then(response => response.json())
+    .then(data => console.log(data[1].submissions))
+  }
+
   render() {
     return (
       <div className="App">
