59d16b58bfb3ad773f4e905ccae62e07cd48718c
Roshanak <rosh.agahi@gmail.com>
2021-08-14 14:11:57 -0700

    import store, middleware, thunk, provider to index.js

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 //import './index.css';
 import App from './App';
 
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
 ReactDOM.render(<App />, document.getElementById('root'));
+ 
