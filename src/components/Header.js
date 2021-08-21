2516c704bb576495ae497b4118f1126f9a11cadd
Roshanak <rosh.agahi@gmail.com>
2021-08-21 14:58:42 -0700

    adding project info to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,12 @@ const Header = () => {
   return (
     <div className="header">
       <img src="/images/office_olympics_logo.png" alt="Logo" />
+      <div>
+        <h3>Project #5: React</h3>
+        <p>August 2021</p>
+        <p>Roshanak Agahi</p>
+      </div>
+      <p>An app designed for companies to host team building challenges for their employees</p>
       <button>New Challenge</button>
     </div>
   )
