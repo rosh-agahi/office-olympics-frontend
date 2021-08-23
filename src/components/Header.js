690a3e5a681fa41c296922b45791230b68070ffc
Roshanak <rosh.agahi@gmail.com>
2021-08-23 16:17:21 -0700

    more header formatting

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,10 +6,11 @@ const Header = () => {
   return (
     <div className="header">
       <img src="/images/office_olympics_logo.png" alt="Logo" />
-      <div className="leftalign">
-        <h2>Project #5: React/Redux -- August 2021 -- Roshanak Agahi</h2>
-        <p className="leftalign">An app designed for companies to host team building challenges for their employees</p>
-        </div>
+      <div className="textbox">
+        <h1>Roshanak Agahi</h1>
+        <h2>Project #5: React/Redux ○ August 2021</h2>
+        <h3>An app designed for companies to host team building challenges for their employees</h3>
+      </div>
     </div>
   )
 }
