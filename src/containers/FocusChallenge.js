9096f244f17a0f735d000f989294b72d43637f6e
Roshanak <rosh.agahi@gmail.com>
2021-08-23 20:44:12 -0700

    FocusChallenge only select challenge if pick > 0 to fix error

--- src/containers/FocusChallenge.js
+++ src/containers/FocusChallenge.js
@@ -1,22 +1,37 @@
 import React from 'react';
 import {connect} from 'react-redux';
+// import {pickChallengeSuccess} from '../actions/selectChallengePick'
 import ThisChallenge from '../components/ThisChallenge'
 import Submissions from '../components/Submissions'
 
-const FocusChallenge = ({challenges, picked}) => {
-  return (
-    <div>
-      <ThisChallenge />
-      <Submissions />
-    </div>
-  )
+class FocusChallenge extends React.Component {
+
+  render() {
+
+    if (this.props.p != 0) {
+      return (
+        <div>
+          <ThisChallenge challenge={this.props.challenge}/>
+          <Submissions />
+        </div>
+      )
+    }
+  }
+
 }
 
 const mapStateToProps = state => {
+  const p = state.pickReducer.pick;
+
+  if (p != 0) {
+    return ({
+      challenge: state.challengeReducer.challenges.filter(c=>c.id === parseInt(state.pickReducer.pick))
+    })
+  }
   return ({
-    challenges: state.challenges,
-    picked: 0
+    pick: p
   })
+
 }
 
-export default connect(mapStateToProps)(FocusChallenge)
+export default connect(mapStateToProps, { })(FocusChallenge)
