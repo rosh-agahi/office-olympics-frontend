dc9a4815f30432247c8bd4c991b548c86f0e6be0
Roshanak <rosh.agahi@gmail.com>
2021-08-25 20:04:34 -0700

    no submissions message

--- src/components/Submissions.js
+++ src/components/Submissions.js
@@ -2,11 +2,14 @@ import React from 'react';
 import SubmissionCard from './SubmissionCard'
 
 const Submissions = ({submissions}) => {
+  if (submissions.length === 0) {
+    return (
+      <div><br /><br />There are no submissions yet. Jump on it and add one!</div>
+    )
+  }
     return (
       <div>
-
         {submissions.map(s => <SubmissionCard key={s.id} submission={s}/>)}
-
       </div>
     )
 
